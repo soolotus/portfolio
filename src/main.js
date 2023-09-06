a03ba38645b4bce7a8e5212080393c77b1611025
SooYeon <soolotus632@gmail.com>
2023-09-05 18:43:51 -0600

    js-arrow

--- src/main.js
+++ src/main.js
@@ -16,3 +16,13 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - scrollY / homeHeight;
 
 });
+
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', () => {
+    if (scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+})
